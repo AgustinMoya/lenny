026102103b10dfacf2cbdd0b4f42cfcc0216a50f
Agustin Moya <agustin.moya@despegar.com>
2019-01-10 11:44:28 -0300

    Guardado de gastos en state

--- gasto-semanal/src/componentes/App.js
+++ gasto-semanal/src/componentes/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import '../css/App.css';
-import Header from './Header';
+import '../css/App.css'
+import Header from './Header'
+import Formulario from './Formulario'
 
 class App extends Component {
-  render() {
+  state = {
+    presupuesto: '',
+    restante: '',
+    gastos: {}
+  }
+  agregarGasto = gasto => {
+    const gastos = { ...this.state.gastos }
+
+    gastos[`gasto${Date.now()}`] = gasto
+
+    this.setState({
+      gastos
+    })
+  }
+
+  render () {
     return (
       <div className='App container'>
-        <Header
-          titulo='Gasto semanal'
-        />
+        <Header titulo='Gasto semanal' />
         <div className='contenido-principal contenido'>
           <div className='row'>
             <div className='one-half column'>
-
+              <Formulario agregarGasto={this.agregarGasto} />
             </div>
-            <div className='one-half column'>
-
-            </div>
+            <div className='one-half column' />
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
