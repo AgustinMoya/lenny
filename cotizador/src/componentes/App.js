aaf9b2d2b2275de38fe69af9fd960b2afc83b9da
Agustin Moya <agustin.moya@despegar.com>
2019-02-01 23:53:48 -0300

    Fix indent

--- cotizador/src/componentes/App.js
+++ cotizador/src/componentes/App.js
@@ -6,61 +6,61 @@ import Resumen from './Resumen';
 
 class App extends Component {
 
-  constructor (props){
-    super(props)
-    this.state = {
-      resultado: '',
-      datos: {}
-    }
-  }
-  
-  cotizarSeguro = (datos) => {
-    console.log(datos)
-
-    const {marca, plan, year} = datos;
-
-    //Agregar una base de 2000
-    let resultado = 2000;
-
-    //Obtener la diferencia de años y restar el 3%
-
-    const diferencia = obtenerDiferenciaAnio(year)
-    resultado -= ((diferencia * 3) * resultado) / 100
-    //Europeo 30%, Americano 15%, asiatico 5% de incremento al valor actual
-
-    resultado = calcularMarca(marca) * resultado
-
-    let incrementoPlan = obtenerPlan(plan)
-
-    resultado = parseFloat(incrementoPlan * resultado).toFixed(2)
-
-    const datosAutos = {
-      marca,
-      plan,
-      year
+    constructor (props){
+        super(props)
+        this.state = {
+            resultado: '',
+            datos: {}
+        }
     }
 
-    this.setState({
-      resultado,
-      datos: datosAutos
-    })
-    
-  }
-  
-  render () {
-    return (
-      <div className='contenedor'>
-        <Header titulo='Cotizador de Seguro de Auto' />
-        <div className='contenedor-formulario'>
-          <Formulario cotizarSeguro={this.cotizarSeguro} />
-          <Resumen
-            datos={this.state.datos}
-            resultado={this.state.resultado}
-          />
-        </div>
-      </div>
-    )
-  }
+    cotizarSeguro = (datos) => {
+        console.log(datos)
+
+        const {marca, plan, year} = datos;
+
+        //Agregar una base de 2000
+        let resultado = 2000;
+
+        //Obtener la diferencia de años y restar el 3%
+
+        const diferencia = obtenerDiferenciaAnio(year)
+        resultado -= ((diferencia * 3) * resultado) / 100
+        //Europeo 30%, Americano 15%, asiatico 5% de incremento al valor actual
+
+        resultado = calcularMarca(marca) * resultado
+
+        let incrementoPlan = obtenerPlan(plan)
+
+        resultado = parseFloat(incrementoPlan * resultado).toFixed(2)
+
+        const datosAutos = {
+            marca,
+            plan,
+            year
+        }
+
+        this.setState({
+            resultado,
+            datos: datosAutos
+        })
+
+    }
+
+    render () {
+        return (
+            <div className='contenedor'>
+                <Header titulo='Cotizador de Seguro de Auto' />
+                <div className='contenedor-formulario'>
+                    <Formulario cotizarSeguro={this.cotizarSeguro} />
+                    <Resumen
+                        datos={this.state.datos}
+                        resultado={this.state.resultado}
+                    />
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App
